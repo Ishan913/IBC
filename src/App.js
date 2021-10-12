aed9eddf863d2ef64336a3466d4b346d757e3aba
Ishan <ishan19308@iiitd.ac.in>
2021-10-12 18:28:18 +0530

    landing page start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="navbar"> 
+        <a class="title">IBC Lottery</a>
+      </div>
+      
+      <div class="infoBoard">
+        
+        <div class="box1 tickets">
+          <p class="box1title">Tickets in game</p>
+          <p class="box1value">3 Tickets</p>
+        </div>
+        
+        <div class="box1 bets">
+          <p class="box1title">Bets Placed</p>
+          <p class="box1value">2.4 eth</p>
+        </div>
+
+        <div class="box1 balance">
+          <p class="box1title">Winnings</p>
+          <p class="box1value">1.1 eth</p>
+        </div>
+
+        <div class="box1 balance">
+          <p class="box1title">Balance</p>
+          <p class="box1value">22.45</p>
+        </div>
+
+      </div>
     </div>
   );
 }
